f72c98a216d9d9b8e4649e8029c34087ae59c238
Ruan Valente <ruanopensuselinux@gmail.com>
2018-11-07 16:37:00 -0300

    Errata correção do desafio a semana #10

--- Aula-10/challenge-10/challenge-10.js
+++ Aula-10/challenge-10/challenge-10.js
@@ -81,7 +81,7 @@
       return false;
     }
     return function(x, y) {
-      if (typeof x !== "number" && typeof y !== "number") {
+      if (typeof x !== "number" || typeof y !== "number") {
         return false;
       }
       return operation[operator](x, y);
