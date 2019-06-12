ee96fdf2f2b547c99f4b8e7815994513ed92c735
Ruan Valente <ruanopensuselinux@gmail.com>
2019-06-12 15:08:28 -0300

    Adicionado o link para o PR do challenge-33 :tada: The End :heart:

--- Aula-33/challenge-33/challenge-33.js
+++ Aula-33/challenge-33/challenge-33.js
@@ -26,4 +26,6 @@ repositório do GitHub, e envie o pull request para lá.
 Depois, envie um pull request no repositório do curso, colocando no console.log
 abaixo a URL do pull request no seu repositório.
 */
-console.log('Link do pull request do seu projeto');
+console.log(
+  'https://github.com/ruanvalente/final-challenge-javascript-ninja/pull/4'
+);
